3acc30524b520a4a18493d9d64405ee735d18d51
unknown <parvathi.kovi@gmail.com>
2019-08-25 22:24:36 +0530

    routing added

--- Bank/src/app/app-routing.module.ts
+++ Bank/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 
-// import { LoginComponent } from './login/login.component';
-import {RouterModule, Routes} from '@angular/router'
-// import {HomeComponent} from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 
 export const routes:Routes = [
-  // {path:'',redirectTo:'home',pathMatch:'full'},
-  // ,{path:"login",component:LoginComponent} 
-  // ,{path:"home",component:HomeComponent}
-  // ,{path:"**",component:HomeComponent}
+   {path:'',redirectTo:'home',pathMatch:'full'},
+ {path:"login",component:LoginComponent},
+ {path:"register",component:RegisterComponent},
+ {path:"home",component:HomeComponent},
+ {path:"**",component:HomeComponent}
 ];
 
 @NgModule({
