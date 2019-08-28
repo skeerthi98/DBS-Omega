d72d5bf5360b0cdefb90614d0d23a57f7636ff4a
Anya-kurapati <53891834+Anya-kurapati@users.noreply.github.com>
2019-08-28 21:51:05 +0530

    Changes made

--- Bank/src/app/send-money/send-money.component.ts
+++ Bank/src/app/send-money/send-money.component.ts
@@ -18,20 +18,105 @@ export class SendMoneyComponent implements OnInit {
   Money:any;
   Message:string;
   OnTransfer:boolean=false;
+  Imps:boolean=false;
+  Neft:boolean=false;
+  Rtgs:boolean=false;
+  Upi:boolean=false;
 
   ngOnInit() {
   }
   TransferType(type:string){
+
+    if(type=='IMPS')
+      this.Imps=true;
+    if(type=='RTGS')
+      this.Rtgs=true;
+    if(type=='NEFT')
+      this.Neft=true;
+    if(type=='UPI')
+      this.Upi=true;
     this.PaymentType=type;
     this.OnSelectPaymentType=true;
   }
-  TransferMoney(accNum:string,money:any){
-    this.Money=money;
-    this.UpdateBalance(this.SendersAccountNumber,"send",this.Money);
-    this.UpdateBalance(this.ReceiversAccountNumber,"receive",this.Money);
-    this.OnTransfer=true;
-    
+  TransferMoneyImps(accountNumber:string,confirmAccountNumber:string,ifsc:string,money:any,purpose:string){
+    if(accountNumber == confirmAccountNumber)
+    {
+      if(ifsc!=null) {
+
+        if(money != null && money < 200000)
+        {
+          this.Money=money;
+          this.UpdateBalance(this.SendersAccountNumber,"send",this.Money);
+          this.UpdateBalance(this.ReceiversAccountNumber,"receive",this.Money);
+          this.OnTransfer=true;
+        }
+        else
+          this.Message="You can only transfer upto 2,00,000 using IMPS";
+    }
+    else
+      this.Message="Please enter IFSC Code";
+    }
+    else
+      this.Message="Please check Benificiary Account Number";
   }
+
+  TransferMoneyNeft(accountNumber:string,confirmAccountNumber:string,ifsc:string,money:any,purpose:string){
+    if(accountNumber == confirmAccountNumber)
+    {
+      if(ifsc != null) {
+      if(money != null  && money < 1000000)
+      {
+        this.Money=money;
+        this.UpdateBalance(this.SendersAccountNumber,"send",this.Money);
+        this.UpdateBalance(this.ReceiversAccountNumber,"receive",this.Money);
+        this.OnTransfer=true;
+      }
+      else
+        this.Message="You can only transfer upto 10,00,000 using NEFT";
+    }
+    else
+      this.Message="Please enter IFSC Code";
+    }
+    else
+      this.Message="Please check Benificiary Account Number";
+  }
+
+  TransferMoneyRtgs(accountNumber:string,confirmAccountNumber:string,ifsc:string,money:any,purpose:string){
+    if(accountNumber == confirmAccountNumber)
+    {
+      if(ifsc != null) {
+      if(money != null  && money > 200000)
+      {
+        this.Money=money;
+        this.UpdateBalance(this.SendersAccountNumber,"send",this.Money);
+        this.UpdateBalance(this.ReceiversAccountNumber,"receive",this.Money);
+        this.OnTransfer=true;
+      }
+      else
+        this.Message="You can only transfer a minimum amount of 2,00,000 using RTGS";
+    }
+    else
+      this.Message="Please enter IFSC Code";
+    }
+    else
+      this.Message="Please check Benificiary Account Number";
+  }
+
+  TransferMoneyUpi(upiId:string,money:any){
+      if(upiId != null){
+        if(money!= null) {
+          this.Money=money;
+          this.UpdateBalance(this.SendersAccountNumber,"send",this.Money);
+          this.UpdateBalance(this.ReceiversAccountNumber,"receive",this.Money);
+          this.OnTransfer=true;
+        }
+        else
+        this.Message="Please enter money";
+      }
+      else
+      this.Message="Please enter UPI ID";
+  }
+
   UpdateBalance(accNum:string,mode:string,money:any){
     if(mode == 'send'){
       if(this.SendersBalance >money)
